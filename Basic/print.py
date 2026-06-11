27aae6fa7099736c16e81cc8498ff0a30ee976fb
matheashok <mathe.ashokkumar@gmail.com>
2026-06-11 14:16:29 +0530

    print

---
+++ Basic/print.py
@@ -0,0 +1,25 @@
+# Text in Python must be inside quotes. You can use either " double quotes or ' single quotes:
+#print("This will work!")
+#print('This will also work!')
+
+# If you forget to put the text inside quotes, Python will give an error:
+#print(This will cause an error)
+
+# By default, the print() function ends with a new line.
+# If you want to print multiple words on the same line, you can use the end parameter:
+# print("Hello World!", end=" ")
+ #print("I will print on the same line.")
+
+# You can also use the print() function to display numbers, , we don't put numbers inside quotes
+print(3)
+print(358)
+print(50000)
+
+# You can also do math inside the print() function
+print(3 + 3)
+print(2 * 5)
+print(50/5)
+print(50%5)
+
+# You can combine text and numbers in one output by separating them with a comma:
+print("I am", 35, "years old.")
