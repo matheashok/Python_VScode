003d3179702f344afd727fbbe3a7c4f30d0169d0
matheashok <mathe.ashokkumar@gmail.com>
2026-06-11 13:28:10 +0530

    indentation

---
+++ Basic/indentation.py
@@ -0,0 +1,7 @@
+# Python uses indentation to indicate a block of code.
+if 5 > 2:
+  print("Five is greater than two!")
+
+# Python will give you an error if you skip the indentation:
+#if 5 > 2:
+#print("Five is greater than two!")
