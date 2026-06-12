e0e71a1d98650a64bf71e4b01c8e524ffcaa1c49
matheashok <mathe.ashokkumar@gmail.com>
2026-06-12 19:44:34 +0530

    datatype

---
+++ Basic/datatype.py
@@ -0,0 +1,158 @@
+# In programming, data type is an important concept.
+
+# Variables can store data of different types, and different types can do different things.
+
+# Python has the following data types built-in by default, in these categories:
+
+'''Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType'''
+
+# You can get the data type of any object by using the type() function:
+
+'''x = 5
+print(type(x))'''
+
+# In Python, the data type is set when you assign a value to a variable:
+# string
+'''x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# int
+'''x = 20
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))'''
+
+# float
+'''x = 20.5
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# complex
+'''x = 1j
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# list 
+'''x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# tuple 
+'''x = ("apple", "banana", "cherry")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# range 
+'''x = range(10)
+
+#display x:
+print(x)
+
+#convert to list to display the content of x:
+print(list(x))'''
+
+# dict 
+'''x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# set 
+''''x = {"apple", "banana", "cherry"}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# frozenset
+'''x = frozenset({"apple", "banana", "cherry"})
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# bool
+'''x = True
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# bytes
+'''x = b"Hello"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# bytearray
+'''x = bytearray(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# memoryview
+'''x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# nonetype
+'''x = None
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+
+
