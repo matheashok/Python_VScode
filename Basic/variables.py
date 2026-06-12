db66f0092d6781a8c820fdf066414ae370eeda75
matheashok <mathe.ashokkumar@gmail.com>
2026-06-12 12:11:15 +0530

    variable

--- Basic/variables.py
+++ Basic/variables.py
@@ -1,4 +1,12 @@
 # In Python, a variable is created when you assign a value to it:
 
+#x = 5
+#y = "Hello, World"
+
+# Variables are containers for storing data values.
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
 x = 5
-y = "Hello, World"
+y = "John"
+print(x)
+print(y)
