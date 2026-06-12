24facf5aeef80f22dc3e98358d375e2ccb4dfcee
matheashok <mathe.ashokkumar@gmail.com>
2026-06-12 19:59:24 +0530

    datatype

--- Basic/datatype.py
+++ Basic/datatype.py
@@ -155,4 +155,135 @@ print(x)
 print(type(x)) '''
 
 
+# Setting the Specific Data Type
+# If you want to specify the data type,
+#  you can use the following constructor functions:
+
+# str
+'''x = str("Hello World")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# int 
+'''x = int(20)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# float 
+'''x = float(20.5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# complex
+'''x = complex(1j)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# list 
+'''x = list(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# tuple 
+'''x = tuple(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# range
+'''x = range(3, 10)
+
+#display x:
+print(x)
+
+#convert to list to display the content of x:
+print(list(x))'''
+
+# dict
+'''x = dict(name="John", age=36)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# set
+'''x = set(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))'''
+
+# frozenset
+'''x = frozenset(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# boolean
+'''x = bool(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# bytes
+'''x = bytes(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# bytearray
+'''x = bytearray(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+# memoryview
+'''x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) '''
+
+
 
