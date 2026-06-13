290c498337f83afe91c3cceb9c9a86a3edf8b79e
matheashok <mathe.ashokkumar@gmail.com>
2026-06-13 15:59:51 +0530

    numbers

--- Basic/numbers.py
+++ Basic/numbers.py
@@ -83,5 +83,16 @@ print(type(c))'''
 
 # Note: You cannot convert complex numbers into another number type.
 
+# Random Number
+# Python does not have a random() function to make a random number, 
+# but Python has a built-in module called random that can be used to make 
+# random numbers:
+# Import the random module, and display a random number from 1 to 9:
+
+'''import random
+
+print(random.randrange(1, 10))'''
+
+
 
 
