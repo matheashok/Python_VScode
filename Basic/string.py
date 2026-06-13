edb45c4d6124170b59678095cc7192f5d1c986f8
matheashok <mathe.ashokkumar@gmail.com>
2026-06-13 16:31:10 +0530

    string

---
+++ Basic/string.py
@@ -0,0 +1,37 @@
+'''Strings in python are surrounded by either single quotation marks, 
+or double quotation marks.
+
+'hello' is the same as "hello".
+
+You can display a string literal with the print() function:'''
+
+'''print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')'''
+
+# Assigning a string to a variable is done with the variable name followed by 
+# an equal sign and the string:
+
+'''a = "Hello"
+print(a)'''
+
+# You can assign a multiline string to a variable by using three quotes:
+# You can use three double quotes:
+'''a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# three single quotes
+b = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(b)'''
+
+# Note: in the result, the line breaks are inserted at the same position as in the code.
+
