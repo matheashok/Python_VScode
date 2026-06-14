21096eaa96d87f14f4de80ca798b7d6f3797bacd
matheashok <mathe.ashokkumar@gmail.com>
2026-06-14 17:49:29 +0530

    string

--- Basic/string.py
+++ Basic/string.py
@@ -31,7 +31,17 @@ b = '''Lorem ipsum dolor sit amet,
 consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua.'''
+
 print(b)'''
 
 # Note: in the result, the line breaks are inserted at the same position as in the code.
 
+# Strings are Arrays
+# Like many other popular programming languages, strings in Python are arrays of unicode characters.
+# However, Python does not have a character data type, a single character is simply a string with a length of 1.
+# Square brackets can be used to access elements of the string.
+
+# Get the character at position 1 (remember that the first character has the position 0):
+
+a = "Hello, World!"
+print(a[1])
