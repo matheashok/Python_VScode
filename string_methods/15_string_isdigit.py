d03b4c044683c2c6402987edff5bef5c5e9326a5
matheashok <mathe.ashokkumar@gmail.com>
2026-06-15 17:18:52 +0530

    string_methods/15_string_isdigit.py

---
+++ string_methods/15_string_isdigit.py
@@ -0,0 +1,24 @@
+# Check if all the characters in the text are digits:
+
+'''txt = "50800"
+
+x = txt.isdigit()
+
+print(x)'''
+
+# Definition and Usage
+# The isdigit() method returns True if all the characters are digits, otherwise False.
+# Exponents, like ², are also considered to be a digit.
+# Syntax : string.isdigit()
+# Parameter Values : No parameters.
+
+# Check if all the characters in the text are digits:
+
+'''a = "\u0030" #unicode for 0
+b = "\u00B2" #unicode for ²
+
+print(a.isdigit())
+print(b.isdigit())'''
+
+
+
