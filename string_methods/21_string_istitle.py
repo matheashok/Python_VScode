cd19014da96193b49246f39e8ae435b24d84835c
matheashok <mathe.ashokkumar@gmail.com>
2026-06-15 17:49:01 +0530

    string_methods/21_string_istitle.py

---
+++ string_methods/21_string_istitle.py
@@ -0,0 +1,27 @@
+# Check if each word start with an upper case letter:
+
+'''txt = "Hello, And Welcome To My World!"
+
+x = txt.istitle()
+
+print(x)'''
+
+# Definition and Usage
+# The istitle() method returns True if all words in a text start with a upper case letter, AND the rest of the word are lower case letters, otherwise False.
+# Symbols and numbers are ignored.
+# Syntax : string.istitle()
+# Parameter Values : No parameters.
+
+# Check if each word start with an upper case letter:
+
+'''a = "HELLO, AND WELCOME TO MY WORLD"
+b = "Hello"
+c = "22 Names"
+d = "This Is %'!?"
+
+print(a.istitle())
+print(b.istitle())
+print(c.istitle())
+print(d.istitle())'''
+
+
