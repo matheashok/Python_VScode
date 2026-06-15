db084414c94a5d653d5883bf91eff93d4c507934
matheashok <mathe.ashokkumar@gmail.com>
2026-06-15 16:04:59 +0530

    string_methods/5_string_encode.py

---
+++ string_methods/5_string_encode.py
@@ -0,0 +1,34 @@
+# UTF-8 encode the string:
+
+'''txt = "My name is Ståle"
+
+x = txt.encode()
+
+print(x)'''
+
+# Definition and Usage
+# The encode() method encodes the string, using the specified encoding. 
+# If no encoding is specified, UTF-8 will be used.
+
+# Syntax : string.encode(encoding=encoding, errors=errors)
+
+# Parameter Values
+# encoding : Optional. A String specifying the encoding to use. Default is UTF-8
+# errors : Optional. A String specifying the error method. Legal values are:
+#           'backslashreplace'	- uses a backslash instead of the character that could not be encoded
+#           'ignore'	- ignores the characters that cannot be encoded
+#           'namereplace'	- replaces the character with a text explaining the character
+#           'strict'	- Default, raises an error on failure
+#           'replace'	- replaces the character with a questionmark
+#           'xmlcharrefreplace'	- replaces the character with an xml character
+
+# These examples uses ascii encoding, and a character that cannot be encoded, 
+# showing the result with different errors:
+
+txt = "My name is Ståle"
+
+print(txt.encode(encoding="ascii",errors="backslashreplace"))
+print(txt.encode(encoding="ascii",errors="ignore"))
+print(txt.encode(encoding="ascii",errors="namereplace"))
+print(txt.encode(encoding="ascii",errors="replace"))
+print(txt.encode(encoding="ascii",errors="xmlcharrefreplace"))
