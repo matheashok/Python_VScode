6f545575c0cd87f9a0555e12a72b1b1f8435a3e2
matheashok <mathe.ashokkumar@gmail.com>
2026-06-15 16:32:08 +0530

    string_methods/8_string_find.py

---
+++ string_methods/8_string_find.py
@@ -0,0 +1,46 @@
+# Where in the text is the word "welcome"?:
+
+'''txt = "Hello, welcome to my world."
+
+x = txt.find("welcome")
+
+print(x)'''
+
+# Definition and Usage
+# The find() method finds the first occurrence of the specified value.
+# The find() method returns -1 if the value is not found.
+# The find() method is almost the same as the index() method, 
+#                   the only difference is that the index() method raises an exception 
+#                   if the value is not found. (See example below)
+
+# Syntax : string.find(value, start, end)
+# Parameter Values
+# value : Required. The value to search for
+# start : Optional. Where to start the search. Default is 0
+# end : Optional. Where to end the search. Default is to the end of the string
+
+
+# Where in the text is the first occurrence of the letter "e"?:
+
+'''txt = "Hello, welcome to my world."
+
+x = txt.find("e")
+
+print(x)'''
+
+# Where in the text is the first occurrence of the letter "e" when you only search between position 5 and 10?:
+
+'''txt = "Hello, welcome to my world."
+
+x = txt.find("e", 5, 10)
+
+print(x)'''
+
+# If the value is not found, the find() method returns -1, but the index() method will raise an exception:
+
+'''txt = "Hello, welcome to my world."
+
+print(txt.find("q"))
+print(txt.index("q"))'''
+
+
