3c40489d630ef9c91a30c540c903fa94a03cc70a
matheashok <mathe.ashokkumar@gmail.com>
2026-06-15 15:55:45 +0530

    string_methods/4_string_count.py

---
+++ string_methods/4_string_count.py
@@ -0,0 +1,25 @@
+# Return the number of times the value "apple" appears in the string:
+
+'''txt = "I love apples, apples are my favorite fruit"
+
+x = txt.count("apple")
+
+print(x)'''
+
+# Definition and Usage
+# The count() method returns the number of times a specified value appears in the string.
+# Syntax : string.count(value, start, end)
+
+# Parameter Values
+# value	: Required. A String. The string to value to search for
+# start	: Optional. An Integer. The position to start the search. Default is 0
+# end:Optional. An Integer. The position to end the search.Default is the end of the string
+
+# Search from position 10 to 24:
+
+'''txt = "I love apples, apple are my favorite fruit"
+
+x = txt.count("apple", 10, 24)
+
+print(x)'''
+
