8d99fa3ce1dd9f8d685d6ca34c28301e9c3eea0d
matheashok <mathe.ashokkumar@gmail.com>
2026-06-15 17:00:12 +0530

    string_methods/11_string_isalnum.py

---
+++ string_methods/11_string_isalnum.py
@@ -0,0 +1,13 @@
+# Check if all the characters in the text are alphanumeric:
+
+'''txt = "Company12"
+
+x = txt.isalnum()
+
+print(x)'''
+
+# The isalnum() method returns True if all the characters are alphanumeric, meaning alphabet letter (a-z) and numbers (0-9).
+# Example of characters that are not alphanumeric: (space)!#%&? etc.
+# Syntax : string.isalnum()
+# Parameter Values : No parameters.
+
