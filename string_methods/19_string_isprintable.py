e9c5d55cb980086bbdbb774a7269f47ab7f90456
matheashok <mathe.ashokkumar@gmail.com>
2026-06-15 17:41:24 +0530

    string_methods/19_string_isprintable.py

---
+++ string_methods/19_string_isprintable.py
@@ -0,0 +1,22 @@
+# Check if all the characters in the text are printable:
+
+'''txt = "Hello! Are you #1?"
+
+x = txt.isprintable()
+
+print(x)'''
+
+# Definition and Usage
+# The isprintable() method returns True if all the characters are printable, otherwise False.
+# Example of none printable character can be carriage return and line feed.
+# Syntax : string.isprintable()
+# Parameter Values : No parameters.
+
+# Check if all the characters in the text are printable:
+
+'''txt = "Hello!\nAre you #1?"
+
+x = txt.isprintable()
+
+print(x)'''
+
