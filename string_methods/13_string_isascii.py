fe3dc306802064a367cdce57d2e0317d71177c88
matheashok <mathe.ashokkumar@gmail.com>
2026-06-15 17:09:04 +0530

    string_methods/13_string_isascii.py

---
+++ string_methods/13_string_isascii.py
@@ -0,0 +1,13 @@
+# Check if all the characters in the text are ascii characters:
+
+txt = "Company123"
+
+x = txt.isascii()
+
+print(x)
+
+# Definition and Usage
+# The isascii() method returns True if all the characters are ascii characters  (a-z).
+# Syntax : string.isascii()
+# Parameter Values : No parameters.
+
