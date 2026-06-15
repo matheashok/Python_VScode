3a8c2fd3ee9a0b9974b2bb75b451a4cbfd30ae8c
matheashok <mathe.ashokkumar@gmail.com>
2026-06-15 17:14:13 +0530

    string_methods/14_string_isdecimal.py

---
+++ string_methods/14_string_isdecimal.py
@@ -0,0 +1,22 @@
+# Check if all the characters in a string are decimals (0-9):
+
+'''txt = "1234"
+
+x = txt.isdecimal()
+
+print(x)'''
+
+# Definition and Usage
+# The isdecimal() method returns True if all the characters are decimals (0-9).
+# This method can also be used on unicode objects. See example below.
+# Syntax :string.isdecimal()
+# Parameter Values : No parameters.
+
+# Check if all the characters in the unicode are decimals:
+
+'''a = "\u0030" #unicode for 0
+b = "\u0047" #unicode for G
+
+print(a.isdecimal())
+print(b.isdecimal())'''
+
