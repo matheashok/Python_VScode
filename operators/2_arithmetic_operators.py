91ada1e029601bf9c5a8949d275b0c40115ee085
matheashok <mathe.ashokkumar@gmail.com>
2026-06-16 13:12:39 +0530

    operators/2_arithmetic_operators.py

---
+++ operators/2_arithmetic_operators.py
@@ -0,0 +1,35 @@
+# Arithmetic operators are used with numeric values to perform common mathematical operations:
+
+# Here is an example using different arithmetic operators:
+
+'''x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)'''
+
+# Python has two division operators:
+
+# / - Division (returns a float)
+# // - Floor division (returns an integer)
+
+# Example : Division always returns a float:
+
+'''x = 12
+y = 5
+
+print(x / y)'''
+
+# Floor division always returns an integer.
+# It rounds DOWN to the nearest integer:
+
+'''x = 12
+y = 5
+
+print(x // y)'''
+
