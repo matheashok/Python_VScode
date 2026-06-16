01b367d02ce1574f0f88d740f56cb4494a8a379f
matheashok <mathe.ashokkumar@gmail.com>
2026-06-16 13:53:57 +0530

    operators/5_comparison_operators.py

---
+++ operators/5_comparison_operators.py
@@ -0,0 +1,20 @@
+# Comparison operators are used to compare two values:
+
+# Comparison operators return True or False based on the comparison:
+
+'''x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)'''
+
+# Python allows you to chain comparison operators:
+
+'''x = 5
+print(1 < x < 10)
+print(1 < x and x < 10)'''
+
