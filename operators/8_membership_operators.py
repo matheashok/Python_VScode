ccfc9a4b6433ebbc34cde7bb43a4349553a68ca1
matheashok <mathe.ashokkumar@gmail.com>
2026-06-16 19:50:30 +0530

    operators/8_membership_operators.py

---
+++ operators/8_membership_operators.py
@@ -0,0 +1,24 @@
+# Membership operators are used to test if a sequence is presented in an object:
+
+# Check if "banana" is present in a list:
+
+''''x = ["apple", "banana"]
+print("banana" in x)'''
+# returns True because a sequence with the value "banana" is in the list
+
+# Check if "pineapple" is NOT present in a list:
+
+'''x = ["apple", "banana"]
+print("pineapple" not in x)'''
+# returns True because a sequence with the value "pineapple" is not in the list
+
+# The membership operators also work with strings:
+
+# Example
+
+'''text = "Hello World"
+
+print("H" in text)
+print("hello" in text)
+print("z" not in text)'''
+
