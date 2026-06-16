eeca53c872459098ff4e3f0a51c2b4e1f5104a41
matheashok <mathe.ashokkumar@gmail.com>
2026-06-16 13:35:58 +0530

    operators/3_assignment_operators.py

---
+++ operators/3_assignment_operators.py
@@ -0,0 +1,72 @@
+# Assignment operators are used to assign values to variables:
+
+'''x = 5
+print(x)'''
+
+'''x = 5
+x += 3
+print(x)'''
+
+'''x = 5
+x -= 3
+print(x)'''
+
+'''x = 5
+x *= 3
+print(x)'''
+
+'''x = 5
+x /= 3
+print(x)'''
+
+'''x = 5
+x%=3
+print(x)'''
+
+'''x = 5
+x//=3
+print(x)'''
+
+'''x = 5
+x **= 3
+print(x)'''
+
+'''x = 5
+x &= 3
+print(x)'''
+
+'''x = 5
+x |= 3
+print(x)'''
+
+'''x = 5
+x ^= 3
+print(x)'''
+
+'''x = 5
+x >>= 3
+print(x)'''
+
+'''x = 5
+x <<= 3
+print(x)'''
+
+'''print(x := 3)'''
+
+# The Walrus Operator
+# Python 3.8 introduced the := operator, known as the "walrus operator". 
+# It assigns values to variables as part of a larger expression:
+# Example : The count variable is assigned in the if statement, and given the value 5:
+
+'''numbers = [1, 2, 3, 4, 5]
+
+if (count := len(numbers)) > 3:
+    print(f"List has {count} elements")'''
+
+
+
+
+
+
+
+
