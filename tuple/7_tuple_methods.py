abf6efc1e37ba73d35956104d541f76f408a3b88
matheashok <mathe.ashokkumar@gmail.com>
2026-06-20 15:03:38 +0530

    tuple/7_tuple_methods.py

---
+++ tuple/7_tuple_methods.py
@@ -0,0 +1,10 @@
+# Tuple Methods
+# Python has two built-in methods that you can use on tuples.
+
+'''
+
+1) count() : Returns the number of times a specified value occurs in a tuple
+
+2) index() : Searches the tuple for a specified value and returns the position of where it was found
+
+'''
