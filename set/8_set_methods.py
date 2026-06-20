ee3321f5e1203ddc71453f025e804f46601d210c
matheashok <mathe.ashokkumar@gmail.com>
2026-06-20 15:11:56 +0530

    set/8_set_methods.py

---
+++ set/8_set_methods.py
@@ -0,0 +1,48 @@
+# Set Methods
+# Python has a set of built-in methods that you can use on sets.
+
+'''
+Method	Shortcut	Description
+
+1) add() : Adds an element to the set
+
+2) clear() : Removes all the elements from the set
+
+3) copy() : Returns a copy of the set
+
+4) difference() : -	 : Returns a set containing the difference between two or more sets
+
+5) difference_update() : -=	 : Removes the items in this set that are also included in another, 
+                                                                                  specified set
+
+6) discard() : Remove the specified item
+
+7) intersection() :	&  : Returns a set, that is the intersection of two other sets
+
+8) intersection_update() :	&=	: Removes the items in this set that are not present in other, 
+                                                                              specified set(s)
+
+9) isdisjoint() : Returns True if NO items of this set is present in another set
+
+10) issubset() : <=	 : Returns True if all items of this set is present in another set
+
+ 	             <	 : Returns True if all items of this set is present in another, larger set
+                  
+11) issuperset() :	>=	: Returns True if all items of another set is present in this set
+
+ 	                >	: Returns True if all items of another, smaller set is present in this set
+                     
+12) pop() : Removes an element from the set
+
+13) remove() : Removes the specified element
+
+14) symmetric_difference() : ^	: Returns a set with the symmetric differences of two sets
+
+15) symmetric_difference_update() :	^=	: Inserts the symmetric differences from this set and another
+
+16) union() : 	|	: Return a set containing the union of sets
+
+17) update() : 	|=	: Update the set with the union of this set and others
+
+
+'''
