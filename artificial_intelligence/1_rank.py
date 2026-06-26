6194923cabcf4e8afae8b67554ec3d654f42462f
matheashok <mathe.ashokkumar@gmail.com>
2026-06-26 13:39:27 +0530

    artificial_intelligence/1_rank.py

---
+++ artificial_intelligence/1_rank.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+A = np.array([[1,2],[2,4]])
+print(np.linalg.matrix_rank(A))
---
+++ artificial_intelligence/2_pytorch.py
@@ -0,0 +1,8 @@
+import torch
+
+x = torch.tensor([1, 2, 3])
+print(x)
+
+'''device = torch.device("cuda")
+x = torch.tensor([1,2,3], device=device)
+print(x)'''
