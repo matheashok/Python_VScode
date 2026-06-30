92c6c9d67c748fdbbd149e664afb2b7add94060f
matheashok <mathe.ashokkumar@gmail.com>
2026-06-30 17:16:48 +0530

    objectorientedprogramming/7_inheritance.py

--- objectorientedprogramming/7_inheritance.py
+++ objectorientedprogramming/7_inheritance.py
@@ -90,7 +90,8 @@ x.printname()'''
     super().__init__(fname, lname)
     self.graduationyear = 2019'''
 
-# In the example below, the year 2019 should be a variable, and passed into the Student class when creating student objects. To do so, add another parameter in the __init__() function:
+# In the example below, the year 2019 should be a variable, and passed into the Student class when 
+#             creating student objects. To do so, add another parameter in the __init__() function:
 
 # Example: Add a year parameter, and pass the correct year when creating objects:
 
